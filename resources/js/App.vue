81f1a23e0e2cfa3c4b59b55c134b038be895c02d
Roman Vavilov <rmvavilov@gmail.com>
2024-07-24 17:41:18 +0300

    Initialize application setup (Laravel/Vue/Tailwind)

---
+++ resources/js/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="p-4 bg-blue-500 text-white">
+        Tailwind/Vue
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+}
+</script>
+
+<style scoped>
+</style>
