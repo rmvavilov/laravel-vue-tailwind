cb86c3d47b5650760d8333784a1e53a4541d4c04
Roman Vavilov <rmvavilov@gmail.com>
2024-07-28 23:44:56 +0300

    fix: update table styles for full height, scroll, responsive design

--- resources/js/components/ScreenOneComponent.vue
+++ resources/js/components/ScreenOneComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="p-4">
+    <div class="p-4 h-full flex flex-col">
         <div class="mb-4 flex items-center">
             <label for="search" class="mr-2">Search:</label>
             <input
@@ -12,9 +12,9 @@
                 class="p-2 border rounded flex-grow"
             />
         </div>
-        <div class="overflow-y-auto" style="max-height: 500px;">
+        <div class="overflow-y-auto flex-grow">
             <table class="min-w-full bg-white">
-                <thead class="bg-gray-200 text-gray-600 sticky">
+                <thead class="bg-gray-200 text-gray-600 sticky top-0">
                 <tr>
                     <th class="w-1/6 py-2 px-4">ID</th>
                     <th class="w-1/3 py-2 px-4">Title</th>
@@ -23,7 +23,7 @@
                 </tr>
                 </thead>
                 <tbody>
-                <tr v-for="record in records" :key="record.id" class="text-center border-t">
+                <tr v-for="record in records" :key="record.id" class="text-center border-t hover:bg-gray-300">
                     <td class="py-2 px-4">{{ record.id }}</td>
                     <td class="py-2 px-4">{{ record.title }}</td>
                     <td class="py-2 px-4">{{ record.status }}</td>
@@ -77,4 +77,3 @@ export default {
     }
 };
 </script>
-
